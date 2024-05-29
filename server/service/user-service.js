2aeab349bd5f62ed032b8c221c20e0b7b78beca1
user <wolf21ru@yandex.ru>
2024-05-29 17:01:47 +0300

    Initial commit

---
+++ server/service/user-service.js
@@ -0,0 +1,48 @@
+const UserModel = require('../models/user-model')
+const bcrypt = require('bcrypt')
+const uuid = require('uuid')
+const mailService = require('./mail-service')
+const tokenService = require('./token-service')
+const UserDto = require('../dtos/user-dto')
+const ApiError = require('../exceptions/api-error')
+
+class UserService {
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({ email })
+    if (candidate) {
+      throw ApiError.badRequest(`Пользователь с ${email} адресом уже существует`)
+    }
+    const hashedPassword = await bcrypt.hash(password, 3)
+    const activationLink = uuid.v4()
+    const user = await UserModel.create({
+      email,
+      password: hashedPassword,
+      activationLink,
+    })
+    await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`)
+    const userDto = new UserDto(user) // id, email, isActivated
+    const tokens = tokenService.generateTokens({ ...userDto })
+    await tokenService.saveToken(userDto.id, tokens.refreshToken)
+
+    return {
+      ...tokens,
+      user: userDto,
+    }
+  }
+  async activate(activationLink) {
+    try {
+      const user = await UserModel.findOne({ activationLink })
+      if (!user) {
+        throw ApiError.badRequest('Ссылка активации не действительная')
+      }
+      user.isActivated = true
+      await user.save()
+    } catch (error) {
+      console.error('Activation error:', error)
+    }
+  }
+}
+
+module.exports = new UserService()
+
+// flesh
